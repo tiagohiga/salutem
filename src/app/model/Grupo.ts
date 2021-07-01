9e3ce25c73e0657b9f93028b6dc70b2068cf4d15
Isaifo <ysoca98@hotmail.com>
2021-07-01 11:17:47 -0300

    Adicionando a model Grupo

---
+++ src/app/model/Grupo.ts
@@ -0,0 +1,11 @@
+//import {Usuario} from '/Usuario';
+//import {Postagem} from '/Postagem'
+export class Grupo{
+    public idGrupo: number
+    public temaGrupo: string
+    public categoriaGrupo: string
+    public descricaoGrupo: string
+    public urlImagemGrupo: string
+   // public usuario : Usuario[]
+   // public postagem : Postagem[]
+}
